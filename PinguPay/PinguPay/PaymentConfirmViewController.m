64e6adddbac19e5538849918e86b64e29c6e9723
Pratham Mehta <prathammehta@outlook.com>
2015-11-29 09:20:59 +0530

    mostly done

--- PinguPay/PinguPay/PaymentConfirmViewController.m
+++ PinguPay/PinguPay/PaymentConfirmViewController.m
@@ -12,6 +12,8 @@
 
 @property (weak, nonatomic) IBOutlet UITableView *listOfMethodsTableView;
 @property (nonatomic, strong) NSArray *titleStrings;
+@property (nonatomic, strong) NSArray *subTitleStrings;
+
 
 @end
 
@@ -31,6 +33,15 @@
                           @"Google Wallet",
                           @"Citi bank"
                           ];
+    
+    self.subTitleStrings = @[
+                             @"Card No: xxxx-1234",
+                             @"Card No: xxxx-5678",
+                             @"Ready to use",
+                             @"Balance: USD 20.56",
+                             @"Balance: USD 320.72",
+                             @"Bank A/C No: xxxx-3543"
+                             ];
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
@@ -81,9 +92,54 @@
           cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"paymentCell"];
+    NSInteger count;
+    if(indexPath.section == 0)
+    {
+        count = indexPath.row;
+    }
+    else if(indexPath.section == 1)
+    {
+        count = 2 + indexPath.row;
+    }
+    else if(indexPath.section == 2)
+    {
+        count = 5 + indexPath.row;
+    }
+    
+    UILabel *title = (UILabel *)[cell.contentView viewWithTag:2];
+    title.text = self.titleStrings[count];
+    
+    UILabel *detail = (UILabel *)[cell.contentView viewWithTag:3];
+    detail.text = self.subTitleStrings[count];
+    
     return cell;
 }
 
+- (IBAction)ConfirmPressed:(UIButton *)sender
+{
+    UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] init];
+    indicator.frame = sender.frame;
+    sender.hidden = YES;
+    [self.view addSubview:indicator];
+    [indicator setColor:[UIColor lightGrayColor]];
+    [indicator startAnimating];
+    double delayInSeconds = 1.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void)
+    {
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Transaction Successful"
+                                                                                 message:@"Your transaction has been successfully processesed. Feel free to close this app."
+                                                                          preferredStyle:UIAlertControllerStyleAlert];
+        [alertController addAction:[UIAlertAction actionWithTitle:@"Okay"
+                                                            style:UIAlertActionStyleCancel
+                                                          handler:nil]];
+        [self presentViewController:alertController animated:YES completion:nil];
+        [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+    });
+}
 
+- (IBAction)cancelPressed:(UIButton *)sender
+{
+}
 
 @end
